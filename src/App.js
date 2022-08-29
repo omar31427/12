fd132113b4d9e1d8d274ed4a4be51c079f23311e
omar abdel al <omarabdelal69@gmail.com>
2022-08-29 10:26:24 +0300

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Change from "./components/Change";
+function App() {
+  return (
+    <div className="App">
+     <Change/>
+    </div>
+  );
+}
+
+export default App;
